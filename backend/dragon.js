86b986bb4048fdb24b812645daeb91f06c9cb92b
Roman Kiss <romiki44@hotmail.com>
2019-05-05 08:51:17 +0200

    First commit

---
+++ backend/dragon.js
@@ -0,0 +1,8 @@
+class Dragon {
+  constructor(bithdate, nickname) {
+    this.bithdate=bithdate;
+    this.nickname=nickname;
+  }
+}
+
+module.exports=Dragon;
---
+++ backend/index.js
@@ -0,0 +1,4 @@
+const Dragon = require('./dragon');
+
+const fooey = new Dragon(new Date(), 'fooey');
+const baloo = new Dragon(new Date(), 'baloo');
