4b28f431e8bf948bc631aa6879e8d0f7b147aacf
Roman Kiss <romiki44@hotmail.com>
2019-05-05 11:46:32 +0200

    before generation completed

---
+++ backend/config.js
@@ -0,0 +1,14 @@
+const SECOND=1000;
+const MINUTES=SECOND*60;
+const HOURS=MINUTES*60;
+const DAYS=HOURS*24;
+
+const REFRESH_RATE=5;
+
+module.exports={
+  SECOND,
+  MINUTES,
+  HOURS,
+  DAYS,
+  REFRESH_RATE
+};
--- backend/dragon.js
+++ backend/dragon.js
@@ -1,8 +1,34 @@
+const TRAITS = require('./traits.json');
+
+const DEFAULT_PROPERTIES = {
+  nickname: 'unnamed',
+  get birthdate() {
+    return new Date();
+  },
+  get randomTraits() {
+    const traits = [];
+
+    TRAITS.forEach(trait => {
+      const traitType = trait.type;
+      const traitValues = trait.values;
+
+      const traitValue = traitValues[
+        Math.floor(Math.random() * traitValues.length)
+      ];
+
+      traits.push({traitType, traitValue});
+    });
+
+    return traits;
+  }
+};
+
 class Dragon {
-  constructor(bithdate, nickname) {
-    this.bithdate=bithdate;
-    this.nickname=nickname;
+  constructor({ birthdate, nickname, traits } = {}) {
+    this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
+    this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
+    this.traits = traits || DEFAULT_PROPERTIES.randomTraits;
   }
 }
 
-module.exports=Dragon;
+module.exports = Dragon;
